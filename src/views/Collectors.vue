248b7770b81c5b8640de797fb332dcda4b03796c
jullemusten <hellstrom.julia@hotmail.com>
2020-11-25 16:00:25 +0100

    fixed format in collectors.vue

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -3,18 +3,27 @@
     <main>
       <div class="buttons">
         <button @click="drawCard">
-          {{ labels.draw }} 
+          {{ labels.draw }}
         </button>
       </div>
       <div class="my-cards">
-        <CollectorsCard v-for="(card, index) in myCards" :card="card" :key="index"/>
+        <CollectorsCard
+          v-for="(card, index) in myCards"
+          :card="card"
+          :key="index"
+        />
       </div>
     </main>
     <footer>
-        <p>
-          {{ labels.invite }}
-          <input type="text" :value="publicPath + $route.path" @click="selectAll" readonly="readonly">
-        </p>
+      <p>
+        {{ labels.invite }}
+        <input
+          type="text"
+          :value="publicPath + $route.path"
+          @click="selectAll"
+          readonly="readonly"
+        />
+      </p>
     </footer>
   </div>
 </template>
@@ -22,48 +31,54 @@
 <script>
 /*eslint no-unused-vars: ["error", { "varsIgnorePattern": "[iI]gnored" }]*/
 
-import CollectorsCard from '@/components/CollectorsCard.vue'
+import CollectorsCard from "@/components/CollectorsCard.vue";
 
 export default {
-  name: 'Collectors',
+  name: "Collectors",
   components: {
-    CollectorsCard
+    CollectorsCard,
   },
   data: function () {
     return {
       publicPath: "localhost:8080/#", //"collectors-groupxx.herokuapp.com/#",
       touchScreen: false,
       myCards: [],
-      maxSizes: { x: 0, 
-                  y: 0 },
+      maxSizes: { x: 0, y: 0 },
       labels: {},
-      points: {}
-    }
+      points: {},
+    };
   },
   created: function () {
-    this.$store.commit('SET_PLAYER_ID', this.$route.query.id)
+    this.$store.commit("SET_PLAYER_ID", this.$route.query.id);
     //TODO! Fix this ugly hack
     //background: https://github.com/quasarframework/quasar/issues/5672
-    const newRoute = this.$route.params.id + "?id=" + this.$store.state.playerId;
+    const newRoute =
+      this.$route.params.id + "?id=" + this.$store.state.playerId;
     if (this.$route.params.id + "?id=" + this.$route.query.id !== newRoute)
       this.$router.push(newRoute);
-    this.$store.state.socket.emit('collectorsLoaded', 
-      { roomId: this.$route.params.id, 
-        playerId: this.$store.state.playerId } );
-    this.$store.state.socket.on('collectorsInitialize', 
-      function(d) {
+    this.$store.state.socket.emit("collectorsLoaded", {
+      roomId: this.$route.params.id,
+      playerId: this.$store.state.playerId,
+    });
+    this.$store.state.socket.on(
+      "collectorsInitialize",
+      function (d) {
         this.labels = d.labels;
         this.myCards = d.hand;
-      }.bind(this));
-    this.$store.state.socket.on('collectorsPointsUpdated', (d) => this.points = d );
+      }.bind(this)
+    );
+    this.$store.state.socket.on(
+      "collectorsPointsUpdated",
+      (d) => (this.points = d)
+    );
 
-    this.$store.state.socket.on('collectorsCardDrawn', 
-      function(d) {
+    this.$store.state.socket.on(
+      "collectorsCardDrawn",
+      function (d) {
         console.log(d);
-        if(d.playerId === this.$route.query.id) {
+        if (d.playerId === this.$route.query.id) {
           this.myCards = d.cards;
-        }
-        else {
+        } else {
           console.log("another player drew a card");
         }
       }.bind(this)
@@ -74,50 +89,52 @@ export default {
       n.target.select();
     },
     drawCard: function () {
-      this.$store.state.socket.emit('collectorsDrawCard', { roomId: this.$route.params.id, 
-           playerId: this.$store.state.playerId });
-    }
+      this.$store.state.socket.emit("collectorsDrawCard", {
+        roomId: this.$route.params.id,
+        playerId: this.$store.state.playerId,
+      });
+    },
   },
-}
+};
 </script>
 <style scoped>
-  header {
-    user-select: none;
-    position: fixed;
-    width:100%;
-    pointer-events: none;
-  }
+header {
+  user-select: none;
+  position: fixed;
+  width: 100%;
+  pointer-events: none;
+}
+main {
+  user-select: none;
+}
+footer {
+  margin-top: 5em auto;
+}
+footer a {
+  text-decoration: none;
+  border-bottom: 2px dotted ivory;
+}
+footer a:visited {
+  color: ivory;
+}
+.my-cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 130px);
+  grid-template-rows: repeat(auto-fill, 180px);
+}
+.my-cards div {
+  transform: scale(0.5) translate(-50%, -50%);
+  transition: 0.2s;
+  transition-timing-function: ease-out;
+  z-index: 0;
+}
+.my-cards div:hover {
+  transform: scale(1) translate(-25%, 0);
+  z-index: 1;
+}
+@media screen and (max-width: 800px) {
   main {
-    user-select: none;
+    width: 90vw;
   }
-  footer {
-    margin-top: 5em auto;
-  }
-  footer a {
-    text-decoration: none;
-    border-bottom: 2px dotted ivory;
-  }
-  footer a:visited {
-    color:ivory;
-  }
-  .my-cards {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 130px);
-    grid-template-rows: repeat(auto-fill, 180px);
-  }
-  .my-cards div {
-    transform: scale(0.5)translate(-50%,-50%);
-    transition:0.2s;
-    transition-timing-function: ease-out;
-    z-index: 0;
-  }
-  .my-cards div:hover {
-    transform: scale(1)translate(-25%,0);
-    z-index: 1;
-  }
-  @media screen and (max-width: 800px) {
-    main {
-      width:90vw;
-    }
-  }
+}
 </style>
