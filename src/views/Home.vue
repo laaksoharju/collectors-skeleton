c57283813c6c3cb95c73fa79cf5f65c39a654674
desolamo <stella.jarlov@gmail.com>
2020-11-27 15:20:20 +0100

    baby

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="center">
     <div>
-      <h1>Collectors</h1>
+      <h1>COLLECTORS</h1>
+      <div>
+          <div> First time player? </div>
+          <div class="popup" onclick="myFunction()">Click me!
+            <span class="popuptext" id="myPopup">Popup text...</span>
+          </div>
+
+        <button id='firstButton'><img src='https://website-bamedag.netdna-ssl.com/fileadmin/media/_processed_/8/8/csm_7-9-months-introtext_7588a3bb0c.jpg' alt="Span" title="Click for rules!" width="200"> </button>
+      </div>
       <ul>
-        <li v-for="i in 3" :key="i">
-          <a @click="setupCollectors(i+1, 'en')">Set up a game of Collectors for {{i+1}} players</a>.
-        </li>
+        <div v-for="i in 3" :key="i">
+          <button id=startButton @click="setupCollectors(i+1, 'en')">Set up a game of Collectors for {{i+1}} players</button>
+        </div>
       </ul>
-      <ul>
+    <!--  <ul>
         <li v-for="i in 3" :key="i">
           <a @click="setupCollectors(i+1, 'se')">Spela Collectors med {{i+1}} spelare</a>.
         </li>
-      </ul>
+      </ul> -->
     </div>
   </div>
 </template>
@@ -26,7 +34,7 @@ export default {
   },
   methods: {
     setupCollectors: function (playerCount, lang="en") {
-      this.$store.commit('SETUP_GAME', {roomId: this.$store.state.roomId, 
+      this.$store.commit('SETUP_GAME', {roomId: this.$store.state.roomId,
         playerCount: playerCount,
         lang: lang });
       this.$router.push("/room/" + this.$store.state.roomId);
@@ -36,10 +44,29 @@ export default {
 </script>
 <style scoped>
   .center {
+    background-color: #11707D;
     display: flex;
     align-items: center;
     justify-content: center;
     height: 100vh;
     padding: 1em;
-  }
+}
+  #startButton {
+cursor:pointer;
+transition-duration: 0.4s;
+}
+
+#firstButton{
+  cursor:pointer;
+  opacity: 0.5;
+}
+
+#firstButton:hover{
+  opacity: 1;
+}
+
+#startButton:hover{
+  background-color: #F89D94;
+}
+
 </style>
