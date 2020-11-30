a15f81e143d520301bd60d73ee08dcc2d6a4ea3d
desolamo <stella.jarlov@gmail.com>
2020-11-30 17:17:13 +0100

    background color

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,10 @@ html {
 	height: 100%;
 }
 body {
+
     font-family: 'Gill Sans', Helvetica, Arial, sans-serif;
-    background: rgb(144,215,223);
-    background: radial-gradient(circle, rgba(144,215,223,1) 0%, rgba(22,56,105,1) 100%);
+    background-color: #5A99A1;
+
 	color: ivory;
 	font-size: 100%;
 	margin:0;
