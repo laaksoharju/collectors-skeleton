44364f4e78da95ee4d082bc03e2f59fc7b1dae96
Nafi Zaman <zamannafi3@gmail.com>
2020-12-01 22:51:27 +0100

    Player page updates without refresh to display all players

--- socketsCollectors.js
+++ socketsCollectors.js
@@ -1,41 +1,65 @@
-function sockets(io, socket, data) {
-    socket.on('setupCollectors', function(d) {
-      data.createRoom(d.roomId, d.playerCount, d.lang);
-    })
-    socket.on('collectorsLoaded', function(d) {
-      socket.join(d.roomId);
-      if (data.joinGame(d.roomId, d.playerId)) {
-        socket.emit('collectorsInitialize', {
-            labels: data.getUILabels(d.roomId),
-            players: data.getPlayers(d.roomId),
-            itemsOnSale: data.getItemsOnSale(d.roomId),
-            marketValues: data.getMarketValues(d.roomId),
-            skillsOnSale: data.getSkillsOnSale(d.roomId),
-            auctionCards: data.getAuctionCards(d.roomId),
-            placements: data.getPlacements(d.roomId)
-          };
-        );
-      }
-    });
-    socket.on('collectorsDrawCard', function(d) {
-      io.to(d.roomId).emit('collectorsCardDrawn',
-        data.drawCard(d.roomId, d.playerId)
-      );
-    });
-    socket.on('collectorsBuyCard', function(d) {
-      data.buyCard(d.roomId, d.playerId, d.card, d.cost)
-      io.to(d.roomId).emit('collectorsCardBought', {
-          playerId: d.playerId,
-          players: data.getPlayers(d.roomId),
-          itemsOnSale: data.getItemsOnSale(d.roomId)
-        }
-      );
-    });
-    socket.on('collectorsPlaceBottle', function(d) {
-      data.placeBottle(d.roomId, d.playerId, d.action, d.cost);
-      io.to(d.roomId).emit('collectorsBottlePlaced', data.getPlacements(d.roomId)
-      );
-    });
+function sockets(io, socket, data)
+{
+  socket.on('setupCollectors', function (d)
+  {
+    data.createRoom(d.roomId, d.playerCount, d.lang);
+  })
+  socket.on('collectorsLoaded', function (d)
+  {
+
+    // socket.on('notifyPlayers', function (d)
+    // {
+    //   io.to(d.roomId).emit('notifyPlayers',
+    //     // data.drawCard(d.roomId, d.playerId)
+    //     data.getPlayers(d.roomId)
+    //   );
+    // });
+
+
+
+    socket.join(d.roomId);
+    if (data.joinGame(d.roomId, d.playerId))
+    {
+      socket.emit('collectorsInitialize', {
+        labels: data.getUILabels(d.roomId),
+        players: data.getPlayers(d.roomId),
+        itemsOnSale: data.getItemsOnSale(d.roomId),
+        marketValues: data.getMarketValues(d.roomId),
+        skillsOnSale: data.getSkillsOnSale(d.roomId),
+        auctionCards: data.getAuctionCards(d.roomId),
+        placements: data.getPlacements(d.roomId)
+      });
+    }
+  });
+
+  socket.on('notifyPlayers', function (d)
+  {
+    io.to(d.roomId).emit('notifyPlayers',
+      data.getPlayers(d.roomId)
+    );
+  });
+  socket.on('collectorsDrawCard', function (d)
+  {
+    io.to(d.roomId).emit('collectorsCardDrawn',
+      data.drawCard(d.roomId, d.playerId)
+    );
+  });
+  socket.on('collectorsBuyCard', function (d)
+  {
+    data.buyCard(d.roomId, d.playerId, d.card, d.cost)
+    io.to(d.roomId).emit('collectorsCardBought', {
+      playerId: d.playerId,
+      players: data.getPlayers(d.roomId),
+      itemsOnSale: data.getItemsOnSale(d.roomId)
+    }
+    );
+  });
+  socket.on('collectorsPlaceBottle', function (d)
+  {
+    data.placeBottle(d.roomId, d.playerId, d.action, d.cost);
+    io.to(d.roomId).emit('collectorsBottlePlaced', data.getPlacements(d.roomId)
+    );
+  });
 }
 
 module.exports = sockets;
--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -2,27 +2,38 @@
   <div>
     <main>
       <div class="collectors-game">
-       <div class="game main-board">A</div>
-       <div class="game player-board">
-         <h2>Player Id {{playerId}} has been assigned {{players[playerId].color}} color</h2>
-         <!-- <div class="current-player">
+        <div class="game main-board">A</div>
+        <div class="game player-board">
+          <h2>
+            Player Id {{ playerId }} has been assigned
+            <!-- {{ players[playerId].color }} color -->
+          </h2>
+          <!-- <div class="current-player">
           Current Player: {{players[playerId]}}
          </div><br>
          Other Players <br> -->
-         {{players}}
-         <!-- <div class="other-players" v-for="(value, key) in players" v-bind:key="key">
-            <div v-if="key != playerId">
-              {{ key }}: {{ value }}
-            </div>
-         </div> -->
-       </div>
+          <!-- {{ players }} -->
+          <div
+            class="other-players"
+            v-for="(value, key) in players"
+            v-bind:key="key"
+          >
+            <div v-if="key != playerId">{{ key }}: {{ value }}</div>
+            <br />
+          </div>
+        </div>
       </div>
     </main>
-      <footer>
-        <p>
-          {{ labels.invite }}
-          <input type="text" :value="publicPath + $route.path" @click="selectAll" readonly="readonly">
-        </p>
+    <footer>
+      <p>
+        {{ labels.invite }}
+        <input
+          type="text"
+          :value="publicPath + $route.path"
+          @click="selectAll"
+          readonly="readonly"
+        />
+      </p>
     </footer>
   </div>
 </template>
@@ -34,7 +45,7 @@
 // import CollectorsBuyActions from '@/components/CollectorsBuyActions.vue'
 
 export default {
-  name: 'Collectors',
+  name: "Collectors",
   // components: {
   //   CollectorsCard
   // },
@@ -42,8 +53,7 @@ export default {
     return {
       publicPath: "localhost:8080/#", //"collectors-groupxx.herokuapp.com/#",
       touchScreen: false,
-      maxSizes: { x: 0,
-                  y: 0 },
+      maxSizes: { x: 0, y: 0 },
       labels: {},
       players: {},
       // playerId: {
@@ -60,46 +70,58 @@ export default {
       auctionPlacement: [],
       marketPlacement: [],
       chosenPlacementCost: null,
-      marketValues: { fastaval: 0,
-                     movie: 0,
-                     technology: 0,
-                     figures: 0,
-                     music: 0 },
+      marketValues: {
+        fastaval: 0,
+        movie: 0,
+        technology: 0,
+        figures: 0,
+        music: 0,
+      },
       itemsOnSale: [],
       skillsOnSale: [],
       auctionCards: [],
-      playerid: 0
-    }
+      playerid: 0,
+    };
   },
   computed: {
-    playerId: function() { return this.$store.state.playerId}
+    playerId: function () {
+      return this.$store.state.playerId;
+    },
   },
   watch: {
-    players: function(newP, oldP) {
-      console.log(Object.keys(newP).length);
-      console.log(Object.keys(oldP).length);
+    players: function (newP, oldP) {
+      // console.log(Object.keys(newP).length);
+      // console.log(Object.keys(oldP).length);
+      console.log(newP, oldP);
       for (let p in this.players) {
-        for(let c = 0; c < this.players[p].hand.length; c += 1) {
+        for (let c = 0; c < this.players[p].hand.length; c += 1) {
           if (typeof this.players[p].hand[c].item !== "undefined")
-          this.$set(this.players[p].hand[c], "available", false);
+            this.$set(this.players[p].hand[c], "available", false);
         }
       }
-    }
+    },
   },
   created: function () {
-    this.$store.commit('SET_PLAYER_ID', this.$route.query.id)
+    this.$store.commit("SET_PLAYER_ID", this.$route.query.id);
     //TODO! Fix this ugly hack
     //background: https://github.com/quasarframework/quasar/issues/5672
     const newRoute = this.$route.params.id + "?id=" + this.playerId;
     if (this.$route.params.id + "?id=" + this.$route.query.id !== newRoute)
       this.$router.push(newRoute);
 
-    this.$store.state.socket.emit('collectorsLoaded',
-      { roomId: this.$route.params.id,
-        playerId: this.playerId } );
+    this.$store.state.socket.emit("collectorsLoaded", {
+      roomId: this.$route.params.id,
+      playerId: this.playerId,
+    });
 
-    this.$store.state.socket.on('collectorsInitialize',
-      function(d) {
+    this.$store.state.socket.emit("notifyPlayers", {
+      roomId: this.$route.params.id,
+      // playerId: this.playerId,
+    });
+
+    this.$store.state.socket.on(
+      "collectorsInitialize",
+      function (d) {
         this.labels = d.labels;
         this.players = d.players;
         this.itemsOnSale = d.itemsOnSale;
@@ -110,28 +132,44 @@ export default {
         this.skillPlacement = d.placements.skillPlacement;
         this.marketPlacement = d.placements.marketPlacement;
         this.auctionPlacement = d.placements.auctionPlacement;
-      }.bind(this));
+      }.bind(this)
+    );
 
-    this.$store.state.socket.on('collectorsBottlePlaced',
-      function(d) {
+    this.$store.state.socket.on(
+      "notifyPlayers",
+      function (d) {
+        // console.log("Hello");
+        this.players = d;
+      }.bind(this)
+    );
+
+    this.$store.state.socket.on(
+      "collectorsBottlePlaced",
+      function (d) {
         this.buyPlacement = d.buyPlacement;
         this.skillPlacement = d.skillPlacement;
         this.marketPlacement = d.marketPlacement;
         this.auctionPlacement = d.auctionPlacement;
-      }.bind(this));
-
-    this.$store.state.socket.on('collectorsPointsUpdated', (d) => this.points = d );
-
-    this.$store.state.socket.on('collectorsCardDrawn',
-      function(d) {
-          //this has been refactored to not single out one player's cards
-          //better to update the state of all cards
-          this.players = d;
       }.bind(this)
     );
 
-    this.$store.state.socket.on('collectorsCardBought',
-      function(d) {
+    this.$store.state.socket.on(
+      "collectorsPointsUpdated",
+      (d) => (this.points = d)
+    );
+
+    this.$store.state.socket.on(
+      "collectorsCardDrawn",
+      function (d) {
+        //this has been refactored to not single out one player's cards
+        //better to update the state of all cards
+        this.players = d;
+      }.bind(this)
+    );
+
+    this.$store.state.socket.on(
+      "collectorsCardBought",
+      function (d) {
         console.log(d.playerId, "bought a card");
         this.players = d.players;
         this.itemsOnSale = d.itemsOnSale;
@@ -142,102 +180,102 @@ export default {
     selectAll: function (n) {
       n.target.select();
     },
+    // notifyPlayers: function () {
+    //   console.log("Hello there");
+    // },
     placeBottle: function (action, cost) {
       this.chosenPlacementCost = cost;
-      this.$store.state.socket.emit('collectorsPlaceBottle', {
-          roomId: this.$route.params.id,
-          playerId: this.playerId,
-          action: action,
-          cost: cost,
-        }
-      );
+      this.$store.state.socket.emit("collectorsPlaceBottle", {
+        roomId: this.$route.params.id,
+        playerId: this.playerId,
+        action: action,
+        cost: cost,
+      });
     },
     drawCard: function () {
-      this.$store.state.socket.emit('collectorsDrawCard', {
-          roomId: this.$route.params.id,
-          playerId: this.playerId
-        }
-      );
+      this.$store.state.socket.emit("collectorsDrawCard", {
+        roomId: this.$route.params.id,
+        playerId: this.playerId,
+      });
     },
     buyCard: function (card) {
       console.log("buyCard", card);
-      this.$store.state.socket.emit('collectorsBuyCard', {
-          roomId: this.$route.params.id,
-          playerId: this.playerId,
-          card: card,
-          cost: this.marketValues[card.market] + this.chosenPlacementCost
-        }
-      );
-    }
+      this.$store.state.socket.emit("collectorsBuyCard", {
+        roomId: this.$route.params.id,
+        playerId: this.playerId,
+        card: card,
+        cost: this.marketValues[card.market] + this.chosenPlacementCost,
+      });
+    },
   },
-}
+};
 </script>
 <style scoped>
-  header {
-    user-select: none;
-    position: fixed;
-    width:100%;
-    pointer-events: none;
-  }
+header {
+  user-select: none;
+  position: fixed;
+  width: 100%;
+  pointer-events: none;
+}
+main {
+  user-select: none;
+}
+footer {
+  margin-top: 5em auto;
+}
+footer a {
+  text-decoration: none;
+  border-bottom: 2px dotted ivory;
+}
+footer a:visited {
+  color: ivory;
+}
+.cardslots {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 130px);
+  grid-template-rows: repeat(auto-fill, 180px);
+}
+.cardslots div {
+  transform: scale(0.5) translate(-50%, -50%);
+  transition: 0.2s;
+  transition-timing-function: ease-out;
+  z-index: 0;
+}
+.cardslots div:hover {
+  transform: scale(1) translate(-25%, 0);
+  z-index: 1;
+}
+
+.collectors-game {
+  display: grid;
+  grid-gap: 5px;
+  grid-template-columns: 60% 40%;
+  background-color: #fff;
+  color: #444;
+}
+
+.game {
+  background-color: #444;
+  color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 150%;
+  height: 90vh;
+}
+
+.main-board {
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.player-board {
+  grid-column: 2;
+  grid-row: 1;
+}
+
+@media screen and (max-width: 800px) {
   main {
-    user-select: none;
+    width: 90vw;
   }
-  footer {
-    margin-top: 5em auto;
-  }
-  footer a {
-    text-decoration: none;
-    border-bottom: 2px dotted ivory;
-  }
-  footer a:visited {
-    color:ivory;
-  }
-  .cardslots {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 130px);
-    grid-template-rows: repeat(auto-fill, 180px);
-  }
-  .cardslots div {
-    transform: scale(0.5)translate(-50%,-50%);
-    transition:0.2s;
-    transition-timing-function: ease-out;
-    z-index: 0;
-  }
-  .cardslots div:hover {
-    transform: scale(1)translate(-25%,0);
-    z-index: 1;
-  }
-
-  .collectors-game {
-     display: grid;
-     grid-gap: 5px;
-     grid-template-columns: 60% 40%;
-     background-color: #fff;
-     color: #444;
-   }
-
-  .game {
-    background-color: #444;
-     color: #fff;
-     border-radius: 5px;
-     padding: 20px;
-     font-size: 150%;
-     height: 90vh;
-  }
-
-  .main-board {
-    grid-column: 1 ;
-    grid-row: 1 ;
-  }
-
-  .player-board {
-    grid-column: 2 ;
-    grid-row: 1 ;
-  }
-
-  @media screen and (max-width: 800px) {
-    main {
-      width:90vw;
-    }
-  }
+}
 </style>
