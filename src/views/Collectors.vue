de455f7cc63be1ef49268416bdca85ec1042e5dc
EgonAxel <egonaxelsson@hotmail.se>
2020-12-01 10:30:30 +0100

    Merge fungar, BuySkill o Grid

--- src/views/Collectors.vue
+++ src/views/Collectors.vue
@@ -12,30 +12,19 @@
         :placement="buyPlacement"
         @buyCard="buyCard($event)"
         @placeBottle="placeBottle('buy', $event)"/>
-        <div class="buttons">
-          <button @click="drawCard">
-            {{ labels.draw }}
-          </button>
-        </div>
+
       </div>
 
-      <div id ='BuySkillDiv' class="cardslots">
-        <h2>Skills</h2>
-        @placeBottle="placeBottle('buyItem', $event)"/>
-      <CollectorsSkillActions v-if="players[playerId]"
-        :labels="labels"
-        :player="players[playerId]"
-        :skillsOnSale="skillsOnSale"
-        :placement="skillPlacement"
-        @getSkill="getSkill($event)"
-        @placeBottle="placeBottle('buySkill', $event)"/>
-      <div class="buttons">
-        <button @click="drawCard">
-          {{ labels.draw }}
-        </button>
-      </div>
-        <CollectorsCard v-for="(card, index) in skillsOnSale" :card="card" :key="index"/>
-      </div>
+      <div id ='BuySkillDiv' >
+        <CollectorsSkillActions v-if="players[playerId]"
+       :labels="labels"
+       :player="players[playerId]"
+       :skillsOnSale="skillsOnSale"
+       :placement="skillPlacement"
+       @getSkill="getSkill($event)"
+       @placeBottle="placeBottle('buySkill', $event)"/>
+
+    </div>
 
       <div id ='AuctionDiv' class="cardslots">
         <h2>Auction</h2>
@@ -58,6 +47,11 @@
         <button v-if="players[playerId]" @click="players[playerId].money += 1">
           fake more money
         </button>
+        <div class="buttons">
+         <button @click="drawCard">
+           {{ labels.draw }}
+         </button>
+        </div>
         <footer>
           <p>
             {{ labels.invite }}
@@ -71,7 +65,6 @@
 
 <script>
 /*eslint no-unused-vars: ["error", { "varsIgnorePattern": "[iI]gnored" }]*/
-
 import CollectorsCard from '@/components/CollectorsCard.vue'
 import CollectorsBuyActions from '@/components/CollectorsBuyActions.vue'
 import CollectorsSkillActions from '@/components/CollectorsSkillActions.vue'
@@ -137,11 +130,9 @@ export default {
     const newRoute = this.$route.params.id + "?id=" + this.playerId;
     if (this.$route.params.id + "?id=" + this.$route.query.id !== newRoute)
       this.$router.push(newRoute);
-
     this.$store.state.socket.emit('collectorsLoaded',
       { roomId: this.$route.params.id,
         playerId: this.playerId } );
-
     this.$store.state.socket.on('collectorsInitialize',
       function(d) {
         this.labels = d.labels;
@@ -155,7 +146,6 @@ export default {
         this.marketPlacement = d.placements.marketPlacement;
         this.auctionPlacement = d.placements.auctionPlacement;
       }.bind(this));
-
     this.$store.state.socket.on('collectorsBottlePlaced',
       function(d) {
         this.buyPlacement = d.buyPlacement;
@@ -163,9 +153,7 @@ export default {
         this.marketPlacement = d.marketPlacement;
         this.auctionPlacement = d.auctionPlacement;
       }.bind(this));
-
     this.$store.state.socket.on('collectorsPointsUpdated', (d) => this.points = d );
-
     this.$store.state.socket.on('collectorsCardDrawn',
       function(d) {
           //this has been refactored to not single out one player's cards
@@ -173,7 +161,6 @@ export default {
           this.players = d;
       }.bind(this)
     );
-
     this.$store.state.socket.on('collectorsCardBought',
       function(d) {
         console.log(d.playerId, "bought a card");
@@ -196,108 +183,6 @@ export default {
     placeBottle: function (action, cost) {
       this.chosenPlacementCost = cost;
       this.$store.state.socket.emit('collectorsPlaceBottle', {
-        y: 0 },
-        labels: {},
-        players: {},
-        // playerId: {
-        //   hand: [],
-        //   money: 1,
-        //   points: 0,
-        //   skills: [],
-        //   items: [],
-        //   income: [],
-        //   secret: []
-        // }
-        buyPlacement: [],
-        skillPlacement: [],
-        auctionPlacement: [],
-        marketPlacement: [],
-        chosenPlacementCost: null,
-        marketValues: { fastaval: 0,
-          movie: 0,
-          technology: 0,
-          figures: 0,
-          music: 0 },
-          itemsOnSale: [],
-          skillsOnSale: [],
-          auctionCards: [],
-          playerid: 0
-        }
-      },
-      computed: {
-        playerId: function() { return this.$store.state.playerId}
-      },
-      watch: {
-        players: function(newP, oldP) {
-          console.log(newP, oldP)
-          for (let p in this.players) {
-            for(let c = 0; c < this.players[p].hand.length; c += 1) {
-              if (typeof this.players[p].hand[c].item !== "undefined")
-              this.$set(this.players[p].hand[c], "available", false);
-            }
-          }
-        }
-      },
-      created: function () {
-        this.$store.commit('SET_PLAYER_ID', this.$route.query.id)
-        //TODO! Fix this ugly hack
-        //background: https://github.com/quasarframework/quasar/issues/5672
-        const newRoute = this.$route.params.id + "?id=" + this.playerId;
-        if (this.$route.params.id + "?id=" + this.$route.query.id !== newRoute)
-        this.$router.push(newRoute);
-
-        this.$store.state.socket.emit('collectorsLoaded',
-        { roomId: this.$route.params.id,
-          playerId: this.playerId } );
-
-          this.$store.state.socket.on('collectorsInitialize',
-          function(d) {
-            this.labels = d.labels;
-            this.players = d.players;
-            this.itemsOnSale = d.itemsOnSale;
-            this.marketValues = d.marketValues;
-            this.skillsOnSale = d.skillsOnSale;
-            this.auctionCards = d.auctionCards;
-            this.buyPlacement = d.placements.buyPlacement;
-            this.skillPlacement = d.placements.skillPlacement;
-            this.marketPlacement = d.placements.marketPlacement;
-            this.auctionPlacement = d.placements.auctionPlacement;
-          }.bind(this));
-
-          this.$store.state.socket.on('collectorsBottlePlaced',
-          function(d) {
-            this.buyPlacement = d.buyPlacement;
-            this.skillPlacement = d.skillPlacement;
-            this.marketPlacement = d.marketPlacement;
-            this.auctionPlacement = d.auctionPlacement;
-          }.bind(this));
-
-          this.$store.state.socket.on('collectorsPointsUpdated', (d) => this.points = d );
-
-          this.$store.state.socket.on('collectorsCardDrawn',
-          function(d) {
-            //this has been refactored to not single out one player's cards
-            //better to update the state of all cards
-            this.players = d;
-          }.bind(this)
-        );
-
-        this.$store.state.socket.on('collectorsCardBought',
-        function(d) {
-          console.log(d.playerId, "bought a card");
-          this.players = d.players;
-          this.itemsOnSale = d.itemsOnSale;
-        }.bind(this)
-      );
-    },
-    methods: {
-      selectAll: function (n) {
-        n.target.select();
-      },
-      placeBottle: function (action, cost) {
-        this.chosenPlacementCost = cost;
-        this.$store.state.socket.emit('collectorsPlaceBottle', {
-
           roomId: this.$route.params.id,
           playerId: this.playerId,
           action: action,
@@ -332,23 +217,7 @@ export default {
         }
       );
     }
-
-        roomId: this.$route.params.id,
-        playerId: this.playerId
-      }
-    );
   },
-  buyCard: function (card) {
-    console.log("buyCard", card);
-    this.$store.state.socket.emit('collectorsBuyCard', {
-      roomId: this.$route.params.id,
-      playerId: this.playerId,
-      card: card,
-      cost: this.marketValues[card.market] + this.chosenPlacementCost
-    }
-  );
-}
-},
 }
 </script>
 
