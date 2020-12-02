2b5f61d9353ec8b6c19d4c879e90aa8135bf656a
LovisaOls <lovisa.olsson.4242@student.uu.se>
2020-12-02 16:06:32 +0100

    Firstplayertoken created

---
+++ src/components/FirstPlayerToken.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class = "firstPlayer"> 
+        <p>1<sup>st</sup></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FirstPlayerToken"
+};
+</script>
+
+<style scoped>
+.firstPlayer{
+    color: black;
+    padding: 2px;
+    border: solid 1px;
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: coral;
+}
+</style>
