24efb805e8d24eecd621de59a848c35ba98e49d3
AlvaB-ux <Alva.Blomkvist.9444@student.uu.se>
2020-12-03 14:38:50 +0100

    Rulesbutton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,33 +8,26 @@
 
           <a id='firstButton' href="#identifier"><img src='https://website-bamedag.netdna-ssl.com/fileadmin/media/_processed_/8/8/csm_7-9-months-introtext_7588a3bb0c.jpg' alt="Span" title="Click for rules!" width="200"></a>
         </div>
-
-
-        <button title="Rules!" type="button" class="rulesButton" v-on:click="ruleFunction">Get the rules here!</button>
-        <div id="ruleContent" ></div>
-
-
       <ul>
         <div v-for="i in 3" :key="i">
-          <button id=startButton @click="setupCollectors(i+1, 'en')"> <h3>Set up a game of Collectors for {{i+1}} players </h3> </button>
+          <button id=startButton @click="setupCollectors(i+1, 'en')"><h3>Set up a game of Collectors for {{i+1}} players </h3></button>
         </div>
 
       </ul>
 
+      <!--Rule button and id for where the text prints out -->
+      <button title="Rules!" type="button" id="rulesButton" class="rulesButton" v-on:click="ruleFunction"><h3>Click here to read the rules!</h3></button>
+      <div id="ruleContent" ></div>
 
-
-      <p>
         <!--- sorry riktigt dålig lösning:)--><br><br><br><br><br><br><br><br>
           <a name="identifier">RULES</a>
       </p>
 
 
+
     </div>
-
   </div>
-
 </template>
-
 <script>
 // @ is an alias to /src
 
@@ -54,7 +47,18 @@ export default {
     ruleFunction: function() {
       console.log("button clicked");
       var placement = document.getElementById("ruleContent");
-      var rules= "Collectors is a game for 2-4 players and the winning player is the one with the most valuable collection of rare collectables in the end. The game board is divided into 5 different areas, buy item, gain skill, start auction, work and raise value. ";
+      var rules=
+      ["Collectors is a game for 2-4 players and the winning player is" ,
+      "the one with the most valuable collection of rare collectables" ,
+      "in the end. The game board is divided into 5 different areas," ,
+      "buy item, gain skill, start auction, work and raise value.",
+      "You use Buy Item if you want to buy one card from the item pool",
+      "(the red ish area) or from your own hand. The cost will depend on",
+      "how many cards there are with the same symbol in the Market pool" ,
+      "(the blue ish area). The cost will also contain the cost for where ",
+      "the bottle was placed."
+
+    ];
       var text= document.createElement("div");
       var output = document.createTextNode(rules);
       text.appendChild(output);
@@ -72,6 +76,7 @@ export default {
     height: 100vh;
     padding: 1em;
 }
+
   #startButton {
 padding: 1px 80px 1px;
 cursor:pointer;
@@ -95,6 +100,19 @@ color:white;
 
 #startButton:hover{
   background-color: #F89D94;
+}
+/*id rulebutton*/
+#rulesButton:hover {
+  background-color: #F89D94;
+}
+
+/*class rulebutton*/
+#rulesButton {
+  padding: 1px 80px 1px;
+  cursor:pointer;
+  transition-duration: 0.4s;
+  background-color: #3B737A ;
+  color:white;
 }
 
 </style>
