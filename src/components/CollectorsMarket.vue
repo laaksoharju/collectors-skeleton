879b6dc1c13e085a56c3ff201b871b72d66aecdf
fannyfr <fanny_fristedt@hotmail.se>
2020-12-09 17:08:59 +0100

    Market compenent

---
+++ src/components/CollectorsMarket.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class = "marketPool">
+    Market Pool
+    <div class = "iconBird"></div>
+    <div class = "iconRobot"></div>
+    <div class = "iconMusic"></div>
+    <div class = "iconFilm"></div>
+    <div class = "iconTech"></div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'CollectorsMarket',
+  /*components: {
+    CollectorsCard
+  },*/
+  props: {
+    labels: Object,
+    player: Object,
+    marketValues: Object,
+    placement: Array
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+
+
+
+<style scoped>
+
+
+.marketPool{
+  grid-column: 3/span 5;
+  grid-row: 11/span 4;
+  width: auto;
+  height: auto;
+  background-color: #c9d5e1;
+  color: black;
+  display: grid;
+  grid-template-columns: repeat(5, 90px);
+  grid-template-rows: repeat(5, 27.5px);
+  }
+
+  .iconBird {
+    width: 30px;
+    height: 40px;
+    background-position:center;
+    background-image: url('/images/iconBird.PNG');
+    background-size: cover;
+    place-self: center;
+    grid-column: 1;
+    grid-row: 10;
+  }
+  .iconRobot {
+    width: 30px;
+    height: 40px;
+    background-image: url('/images/iconRobot.PNG');
+    background-size: cover;
+    place-self: center;
+    grid-column: 2;
+    grid-row: 10;
+  }
+
+  .iconMusic {
+    width: 30px;
+    height: 40px;
+    background-image: url('/images/iconMusic.PNG');
+    background-size: cover;
+    place-self: center;
+    grid-column: 3;
+    grid-row: 10;
+  }
+  .iconFilm {
+    width: 30px;
+    height: 40px;
+    background-image: url('/images/iconFilm.PNG');
+    background-size: cover;
+    place-self: center;
+    grid-column: 4;
+    grid-row: 10;
+  }
+  .iconTech {
+    width: 30px;
+    height: 40px;
+    background-image: url('/images/iconTech.PNG');
+    background-size: cover;
+    place-self: center;
+    grid-column: 5;
+    grid-row: 10;
+  }
+
+  </style>
