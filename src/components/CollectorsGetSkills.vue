025376c1daaff790a6a70f47879488fdcfaca085
Siricool <siri.nordlander@hotmail.com>
2020-12-09 16:11:41 +0100

    get skills knappar

--- src/components/CollectorsGetSkills.vue
+++ src/components/CollectorsGetSkills.vue
@@ -1,17 +1,33 @@
 <template>
   <div class = "skillPool">
     Skill Pool
-    <div class = "EnergyBottles">
-    </div>
-    <div class = "EnergyBottles">
-    </div>
-    <div class = "EnergyBottles">
-    </div>
 
-    <div class = "EnergyBottleCoin">
+
+<div class="buttons" v-for="(p, index) in placement" :key="index">
+  <button v-if="p.playerId===null & p.cost===0"
+  :disabled="cannotAfford(p.cost)"
+  @click="placeBottle(p)">
+    <div class = "EnergyBottles">
     </div>
-    <div class = "EnergyBottleCoin">
+  </button>
+
+  <button v-if="p.playerId===null & p.cost===1"
+  :disabled="cannotAfford(p.cost)"
+  @click="placeBottle(p)">
+    <div class = "EnergyBottlesCoin">
     </div>
+  </button>
+
+
+<div v-if="p.playerId !== null">
+  {{p.playerId}}
+</div>
+
+</div>
+
+
+
+
 
       <div class = "get-skill" >
         <div v-for="(card, index) in skillsOnSale" :key="index">
@@ -37,20 +53,43 @@ export default {
     labels: Object,
     player: Object,
     skillsOnSale: Array,
-    //placement: Array
+    marketValues: Object,
+    placement: Array
   },
   methods: {
     getSkill: function (card) {
-      this.highlightAvailableCards()
+
       if (card.available){
         this.$emit('getSkill', card)
+        this.highlightAvailableCards()
       }
     },
-   highlightAvailableCards: function (){
+    cannotAfford: function (cost) {
+      let minCost = 100;
+      for(let key in this.marketValues) {
+        if (cost + this.marketValues[key] < minCost)
+          minCost = cost + this.marketValues[key]
+      }
+      return (this.player.money < minCost);
+    },
+    cardCost: function (card) {
+      return this.marketValues[card.market];
+    },
+    placeBottle: function (p) {
+      this.$emit('placeBottle', p.cost);
+      this.highlightAvailableCards(p.cost);
+    },
+    highlightAvailableCards: function (cost=100) {
       for (let i = 0; i < this.skillsOnSale.length; i += 1) {
+        if (this.marketValues[this.skillsOnSale[i].item] <= this.player.money - cost) {
           this.$set(this.skillsOnSale[i], "available", true);
-          }
         }
+        else {
+          this.$set(this.skillsOnSale[i], "available", false);
+        }
+        this.chosenPlacementCost = cost;
+      }
+    }
     }
   }
 </script>
@@ -64,10 +103,12 @@ export default {
   height: auto;
   background-color: #dce5cc;
   color: black;
+  padding-left: 10px;
   display: grid;
   grid-template-columns: repeat(3, 50px);
   grid-template-rows: repeat(6,50px);
   grid-row-gap: 25px;
+  grid-column-gap: 10px;
   grid-auto-flow: column;
   }
 
@@ -78,7 +119,7 @@ export default {
   background-size: cover;
 }
 
-.EnergyBottleCoin{
+.EnergyBottlesCoin{
   width:50px;
   height:50px;
   background-image:  url('/images/Gain-skill-bottle-coin.png');
@@ -88,7 +129,17 @@ export default {
   transform: scale(0.25);
 }
 .get-skill div:hover{
-  transform: scale(2)translate(-25%,0);
+  transform: scale(1.25)translate(15%,0);
+  z-index: 1;
+}
+
+.buttons{
+  background-color: #dce5cc;
+  border-color: #dce5cc;
+}
+
+.buttons div:hover {
+  transform: scale(1.5)translate(0,0);
   z-index: 1;
 }
 </style>
