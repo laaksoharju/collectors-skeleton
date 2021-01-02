61d5422e2d6696ece8b818286356ed3b8a9a829b
Fredrika-B <fredrika.brink.7629@student.uu.se>
2021-01-02 11:52:05 +0100

    playerboard fixad

--- dataHandlerCollectors.js
+++ dataHandlerCollectors.js
@@ -115,13 +115,13 @@ Data.prototype.joinGame = function (roomId, playerId) {
     }
     else if (Object.keys(room.players).length < room.playerCount) {
       console.log("Player", playerId, "joined for the first time");
-      room.players[playerId] = { hand: room.deck.splice(0, 3),
+      room.players[playerId] = { hand: room.deck.splice(0, 2),
                                  money: 1,
                                  points: 0,
                                  skills: [],
                                  items: [],
                                  income: [],
-                                 secret: [],
+                                 secret: room.deck.splice(0,1),
                                  bids: 0,
                                  color: '',
                                  playerBottles: 0,
